0f9317562dac298d447728028a7ff629833b212a
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-04-01 22:44:24 +0200

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -3,15 +3,13 @@ package com.epam.demo;
 import com.epam.utils.StringUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        StringUtils su = new StringUtils();
-        for (String s : args) {
-            if (su.isPositiveNumber(s) == false) {
-                return false;
-            }
-        }
-        return true;
+        Optional<String> firstNegative = args.stream()
+                .filter(num -> !StringUtils.isPositiveNumber(num))
+                .findFirst();
+        return firstNegative.isEmpty();
     }
 }
