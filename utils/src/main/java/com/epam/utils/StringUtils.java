da9311357028041b0f1a82e446a8e17bd2b2b50a
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-04-01 22:39:54 +0200

    done

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,9 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         if (Integer.parseInt(str) > 0) {
-             return true;
-         }
-         return false;
+         return Integer.parseInt(str) > 0;
     }
 }
